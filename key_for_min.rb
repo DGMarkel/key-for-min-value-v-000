2a0b1a92ea38d0472def2e1f7732e487ebd21991
David Markel <davidgmarkel@gmail.com>
2018-03-15 17:27:01 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,12 +1,8 @@
-
-
 def key_for_min_value(name_hash)
   min_key = nil
   min_value = nil
 
-  if name_hash.empty?
-    nil
-  end
+  
 
   name_hash.collect do |name, value|
     if min_value == nil || value < min_value
